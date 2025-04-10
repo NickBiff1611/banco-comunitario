404a059a15868a7ee16f15acb7fc126db4ef4927
NickBiff1611 <biffnicolas7@gmail.com>
2025-04-10 09:38:18 -0300

    Update IConta.ts

---
+++ IConta.ts
@@ -0,0 +1,12 @@
+import { ICliente } from './ICliente';
+
+export interface IConta {
+  numero: string;
+  cliente: ICliente;
+  saldo: number;
+
+  depositar(valor: number): void;
+  sacar(valor: number): void;
+  transferir(valor: number, destino: IConta): void;
+  verificarSaldo(): number;
+}
