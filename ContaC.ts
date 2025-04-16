c4a39dbba66ac3610e92d6c8753c5e94524bbd50
NickBiff1611 <biffnicolas7@gmail.com>
2025-04-16 07:29:44 -0300

    ContaC.ts

--- ContaC.ts
+++ ContaC.ts
@@ -1,12 +1,9 @@
 import { Conta } from './Conta';
 
-export class ContaCorrente extends Conta {
-  private readonly limiteChequeEspecial: number = 100;
+export class ContaCorrente implements Conta {
 
   sacar(valor: number): void {
-    if (valor <= 0) throw new Error('Valor inválido para saque');
     if (valor > this.saldo + this.limiteChequeEspecial) {
-      throw new Error('Saldo insuficiente + limite do cheque especial excedido');
     }
     this.saldo -= valor;
   }
